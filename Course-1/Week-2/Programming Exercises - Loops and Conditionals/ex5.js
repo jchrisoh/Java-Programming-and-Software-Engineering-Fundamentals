04070b1fc83653be7fa9232f83775523e3c32cb9
Chris Oh <86143600+jechangoh@users.noreply.github.com>
2023-01-08 17:27:17 -0500

    Create ex5.js

---
+++ Course-1/Week-2/Programming Exercises - Loops and Conditionals/ex5.js
@@ -0,0 +1,11 @@
+var image = new SimpleImage("chapel.png");
+
+for (var pixel of image.values()) {
+    if (pixel.getX() <= 50 && pixel.getY() <= 50) {
+        pixel.setRed(0);
+        pixel.setGreen(255);
+        pixel.setBlue(0);
+    }
+}
+
+print(image);
