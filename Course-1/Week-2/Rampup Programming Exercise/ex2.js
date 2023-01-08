631dfaa50e2818bc73c61e3e02588e8f75d75598
Chris Oh <86143600+jechangoh@users.noreply.github.com>
2023-01-07 21:57:15 -0500

    Delete Course-1/Week-2/Rampup Programming Exercise directory

--- Course-1/Week-2/Rampup Programming Exercise/ex2.js
+++
@@ -1,8 +0,0 @@
-function reformatName(first, last) {
-    return last + ", " + first;
-}
-
-var result = reformatName("Susan", "Rodger");
-print(result);
-result = reformatName("Robert", "Duvall");
-print(result);
--- Course-1/Week-2/Rampup Programming Exercise/ex4.js
+++
@@ -1,9 +0,0 @@
-function perimeter(imageName) {
-    var someImg = new SimpleImage(imageName);
-    var height = someImg.getHeight();
-    var width = someImg.getWidth();
-    
-    return 2 * (height + width);
-}
-
-print(perimeter("rodger.png"));
