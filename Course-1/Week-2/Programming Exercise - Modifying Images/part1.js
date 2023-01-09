ad09dcf505e71284b04bf24ad9e34a1ec10b3933
Chris Oh <86143600+jechangoh@users.noreply.github.com>
2023-01-08 22:31:07 -0500

    Update part1.js

--- Course-1/Week-2/Programming Exercise - Modifying Images/part1.js
+++ Course-1/Week-2/Programming Exercise - Modifying Images/part1.js
@@ -1,1 +1,16 @@
+var picture = new SimpleImage("hilton.jpg");
+var width = picture.getWidth();
 
+for (var pixel of picture.values()) {
+    if (pixel.getX() < width / 3) {
+        pixel.setRed(255);
+    }
+    else if (pixel.getX() > width / 3 && pixel.getX() < 2 * width / 3) {
+        pixel.setGreen(255);
+    }
+    else {
+        pixel.setBlue(255);
+    }
+}
+
+print(picture);
