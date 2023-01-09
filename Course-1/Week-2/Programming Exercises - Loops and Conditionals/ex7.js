efcb983cd9daea7603d0ce4bc3c99b1158988e5a
Chris Oh <86143600+jechangoh@users.noreply.github.com>
2023-01-08 20:27:56 -0500

    Create ex7.js

---
+++ Course-1/Week-2/Programming Exercises - Loops and Conditionals/ex7.js
@@ -0,0 +1,20 @@
+function changeRed(width, height) {
+    var picture = new SimpleImage(width, height);
+    var red = 0;
+
+    for (var pixel of picture.values()) {
+        pixel.setRed(red);
+        if (red < 256) {
+            red = red + 1;
+        }
+        else {
+            red = 0;
+            red = red + 1;
+        }
+    }
+
+    return picture;
+}
+
+var result = changeRed(256,200);
+print(result);
