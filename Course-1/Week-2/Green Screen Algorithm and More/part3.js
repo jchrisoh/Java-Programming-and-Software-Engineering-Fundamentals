4dbfd7f367b55d534bad3d3ec46b774cf4e2fc65
Chris Oh <86143600+jechangoh@users.noreply.github.com>
2023-01-10 00:20:13 -0500

    Create part3.js

---
+++ Course-1/Week-2/Green Screen Algorithm and More/part3.js
@@ -0,0 +1,22 @@
+function setBlack(pixel) {
+    pixel.setRed(0);
+    pixel.setGreen(0);
+    pixel.setBlue(0);
+}
+
+function addBorder(image, thickness) {
+    for (var pixel of image.values()) {
+        if (pixel.getX() < thickness || image.getWidth() - pixel.getX() <= thickness) {
+            setBlack(pixel);
+        }
+        else if (pixel.getY() < thickness || image.getHeight() - pixel.getY() <= thickness) {
+            setBlack(pixel);
+        }
+    }
+    
+    return image;
+}
+
+var picture = new SimpleImage("smallpanda.png");
+addBorder(picture, 10);
+print(picture);
